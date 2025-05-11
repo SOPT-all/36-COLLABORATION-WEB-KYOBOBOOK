bc9438a48b1177f88d9979341b3c7079b103b7b9
승우 <hsu235@naver.com>
2025-05-12 01:13:08 +0900

    style: 폰트 스타일 시스템 정의
    style: 폰트 스타일 시스템 정의

---
+++ src/styles/fonts.ts
@@ -0,0 +1,101 @@
+export const fonts = {
+    // Header
+    header1: `
+      font-family: "Pretendard";
+      font-weight: 700;
+      font-size: 1.375rem;
+      line-height: 1.4;
+    `,
+    
+    // Title
+    title1: `
+      font-family: "Pretendard";
+      font-weight: 700;
+      font-size: 1.25rem;
+      line-height: 1.4;
+    `,
+    title2: `
+      font-family: "Pretendard";
+      font-weight: 500;
+      font-size: 1.25rem;
+      line-height: 1.4;
+    `,
+    title3: `
+      font-family: "Pretendard";
+      font-weight: 600;
+      font-size: 1.125rem;
+      line-height: 1.4;
+    `,
+  
+    // Sub title
+    subtitle1: `
+      font-family: "Pretendard";
+      font-weight: 700;
+      font-size: 1rem;
+      line-height: 1.4;
+    `,
+    subtitle2: `
+      font-family: "Pretendard";
+      font-weight: 600;
+      font-size: 1rem;
+      line-height: 1.4;
+    `,
+    subtitle3: `
+      font-family: "Pretendard";
+      font-weight: 500;
+      font-size: 1rem;
+      line-height: 1.4;
+    `,
+    subtitle4: `
+      font-family: "Pretendard";
+      font-weight: 700;
+      font-size: 0.875rem;
+      line-height: 1.4;
+    `,
+  
+    // Body
+    body1: `
+      font-family: "Pretendard";
+      font-weight: 600;
+      font-size: 0.875rem;
+      line-height: 1.4;
+    `,
+    body2: `
+      font-family: "Pretendard";
+      font-weight: 500;
+      font-size: 0.875rem;
+      line-height: 1.4;
+    `,
+    body3: `
+      font-family: "Pretendard";
+      font-weight: 400;
+      font-size: 0.875rem;
+      line-height: 1.6;
+    `,
+
+    // Caption
+    caption1: `
+      font-family: "Pretendard";
+      font-weight: 700;
+      font-size: 0.75rem;
+      line-height: 1.4;
+    `,
+    caption2: `
+      font-family: "Pretendard";
+      font-weight: 600;
+      font-size: 0.75rem;
+      line-height: 1.4;
+    `,
+    caption3: `
+      font-family: "Pretendard";
+      font-weight: 500;
+      font-size: 0.75rem;
+      line-height: 1.4;
+    `,
+    caption4: `
+      font-family: "Pretendard";
+      font-weight: 500;
+      font-size: 0.625rem;
+      line-height: 1.4;
+    `,
+};
