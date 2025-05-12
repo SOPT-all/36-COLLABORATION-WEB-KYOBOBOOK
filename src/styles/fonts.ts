20961de136911fa9200df02deaa14ffd0dc38be1
승우 <hsu235@naver.com>
2025-05-13 02:42:13 +0900

    style: rem 단위 기준에 맞춰 font size 리디자인
    style: rem 단위 기준에 맞춰 font size 리디자인

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,101 +1,101 @@
 export const fonts = {
-    // Header
-    header1: `
+  // Header
+  header1: `
       font-family: "Pretendard";
       font-weight: 700;
-      font-size: 1.375rem;
+      font-size: 2.4rem;
       line-height: 1.4;
     `,
-    
-    // Title
-    title1: `
+
+  // Title
+  title1: `
       font-family: "Pretendard";
       font-weight: 700;
-      font-size: 1.25rem;
+      font-size: 2rem;
       line-height: 1.4;
     `,
-    title2: `
+  title2: `
       font-family: "Pretendard";
       font-weight: 500;
-      font-size: 1.25rem;
+      font-size: 2rem;
       line-height: 1.4;
     `,
-    title3: `
+  title3: `
       font-family: "Pretendard";
       font-weight: 600;
-      font-size: 1.125rem;
+      font-size: 1.8rem;
       line-height: 1.4;
     `,
-  
-    // Sub title
-    subtitle1: `
+
+  // Sub title
+  subtitle1: `
       font-family: "Pretendard";
       font-weight: 700;
-      font-size: 1rem;
+      font-size: 1.6rem;
       line-height: 1.4;
     `,
-    subtitle2: `
+  subtitle2: `
       font-family: "Pretendard";
       font-weight: 600;
-      font-size: 1rem;
+      font-size: 1.6rem;
       line-height: 1.4;
     `,
-    subtitle3: `
+  subtitle3: `
       font-family: "Pretendard";
       font-weight: 500;
-      font-size: 1rem;
+      font-size: 1.6rem;
       line-height: 1.4;
     `,
-    subtitle4: `
+  subtitle4: `
       font-family: "Pretendard";
       font-weight: 700;
-      font-size: 0.875rem;
+      font-size: 1.4rem;
       line-height: 1.4;
     `,
-  
-    // Body
-    body1: `
+
+  // Body
+  body1: `
       font-family: "Pretendard";
       font-weight: 600;
-      font-size: 0.875rem;
+      font-size: 1.4rem;
       line-height: 1.4;
     `,
-    body2: `
+  body2: `
       font-family: "Pretendard";
       font-weight: 500;
-      font-size: 0.875rem;
+      font-size: 1.4rem;
       line-height: 1.4;
     `,
-    body3: `
+  body3: `
       font-family: "Pretendard";
       font-weight: 400;
-      font-size: 0.875rem;
+      font-size: 1.4rem;
       line-height: 1.6;
     `,
 
-    // Caption
-    caption1: `
+  // Caption
+  caption1: `
       font-family: "Pretendard";
       font-weight: 700;
-      font-size: 0.75rem;
+      font-size: 1.2rem;
       line-height: 1.4;
     `,
-    caption2: `
+  caption2: `
       font-family: "Pretendard";
       font-weight: 600;
-      font-size: 0.75rem;
+      font-size: 1.2rem;
       line-height: 1.4;
     `,
-    caption3: `
+  caption3: `
       font-family: "Pretendard";
       font-weight: 500;
-      font-size: 0.75rem;
+      font-size: 1.2rem; 
       line-height: 1.4;
     `,
-    caption4: `
+  caption4: `
       font-family: "Pretendard";
       font-weight: 500;
-      font-size: 0.625rem;
+      font-size: 1rem; 
       line-height: 1.4;
     `,
 };
