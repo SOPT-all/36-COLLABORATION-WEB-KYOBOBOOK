3e3d7ceef30dc95c0d99028c7282d002fc5ab076
승우 <hsu235@naver.com>
2025-05-13 03:15:37 +0900

    fix: 잘못된 경로 및 컴포넌트명 오타 수정
    fix: 잘못된 경로 및 컴포넌트명 오타 수정

--- src/pages/HomeBestDetailCart/HomeBestDetailCart.tsx
+++ src/pages/HomeBestDetailCart/HomeBestDetailCart.tsx
@@ -1,5 +1,5 @@
-const HomeBestDetailCwart = () => {
+const HomeBestDetailCart = () => {
   return <div>홈 베스트 차트 상세 페이지입니다</div>;
 };
 
-export default HomeBestDetailCwart;
+export default HomeBestDetailCart;
