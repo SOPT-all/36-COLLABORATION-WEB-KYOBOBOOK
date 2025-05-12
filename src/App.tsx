30c550fba60d68090f1553ee00be4e1e9cfd5faa
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-05-13 03:25:47 +0900

    chore: 절대 경로로 변경 (#14)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@emotion/react';
-import { theme } from './styles/theme';
-import GlobalStyles from './styles/GlobalStyle';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import { theme } from '@/styles/theme';
+import GlobalStyles from '@/styles/GlobalStyle';
 import { queryClient } from '@/utils/query-client';
 import pageRoutes from '@/routes/pageRoutes';
 
