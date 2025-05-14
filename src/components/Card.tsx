8928b5e7b09016e71c7885f30faba0f8fa909a1f
Saebom <spring180@naver.com>
2025-05-15 00:54:48 +0900

    fix: alt 속성 추가 (#17)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export default function Card({ imgUrl, title, type = 'rectangular' }: CardType) 
 
   return (
     <div css={cardStyle}>
-      <img css={imgStyle(imgHeight)} src={imgUrl} />
+      <img css={imgStyle(imgHeight)} src={imgUrl} alt={`책 표지 이미지 - ${title}`} />
       <p css={titleStyle(theme)}>{title}</p>
     </div>
   );
