e0fd93e548ce41ef9a9496d4eb819cd300a46905
Saebom <spring180@naver.com>
2025-05-16 17:54:48 +0900

    feat: chore: sample 데이터 추가 (#28)

--- src/pages/HomeBestDetail/HomeBestDetail.tsx
+++ src/pages/HomeBestDetail/HomeBestDetail.tsx
@@ -1,5 +1,18 @@
+import AuthInfo from '@/pages/HomeBestDetail/components/sections/AuthorInfo';
+import BookDescription from '@/pages/HomeBestDetail/components/sections/BookDescription';
+import BookReview from '@/pages/HomeBestDetail/components/sections/BookReview';
+import BookSummary from '@/pages/HomeBestDetail/components/sections/BookSummary';
+
 const HomeBestDetail = () => {
-  return <div>홈 베스트 상세 페이지입니다</div>;
+  return (
+    <>
+      <p>홈 베스트 상세 페이지입니다</p>
+      <BookSummary />
+      <BookDescription />
+      <AuthInfo />
+      <BookReview />
+    </>
+  );
 };
 
 export default HomeBestDetail;
---
+++ src/pages/HomeBestDetail/components/sections/BookReview/index.tsx
@@ -0,0 +1,9 @@
+import ReviewCard from '@/pages/HomeBestDetail/components/elements/ReviewCard/ReviewCard';
+
+export default function BookReview() {
+  return (
+    <>
+      <ReviewCard />
+    </>
+  );
+}
