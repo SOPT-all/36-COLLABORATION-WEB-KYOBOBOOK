9fe1a44cc07fdb13db03ca397c45feab1410abd0
승우 <hsu235@naver.com>
2025-05-19 02:01:29 +0900

    feat: 장바구니 페이지 헤더 및 서브헤더 제작 (#38)
    feat: 장바구니 페이지 헤더 및 서브헤더 제작

--- src/pages/HomeBestDetailCart/HomeBestDetailCart.tsx
+++ src/pages/HomeBestDetailCart/HomeBestDetailCart.tsx
@@ -1,5 +1,70 @@
+import { useNavigate } from 'react-router-dom';
+import { HomeBestDetailCartStyle as s } from '@/pages/HomeBestDetailCart/HomeBestDetailCart.style';
+
+import Icon from '@/components/Icon';
+// import Chip from '@/components/Chip/Chip';
+
+// import FirstCartView from './components/FirstCartView';
+// import SecondCartView from './components/SecondCartView';
+// import ThirdCartView from './components/ThirdCartView';
+// import FourthCartView from './components/FourthCartView';
+
+import routePath from '@/routes/routePath';
 const HomeBestDetailCart = () => {
-  return <div>홈 베스트 차트 상세 페이지입니다</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => navigate(routePath.HOME_BEST_DETAIL);
+  const handleHome = () => navigate(routePath.HOME);
+
+  return (
+    <div css={s.Wrapper}>
+      <header css={s.Header}>
+        <div css={s.headerTop}>
+          <div css={s.leftButtons}>
+            <button onClick={handleBack} css={s.iconButton}>
+              <Icon name="left" width={28} height={28} />
+            </button>
+            <button onClick={handleHome} css={s.iconButton}>
+              <Icon name="home2" width={28} height={28} />
+            </button>
+          </div>
+          <h1 css={s.cartTitle}>장바구니 (1)</h1>
+          <button css={s.switchButton}>
+            <Icon name="change" width={20} height={20} />
+            <span>바로드림만</span>
+          </button>
+        </div>
+      </header>
+
+      <div css={s.subHeader}>
+        <div css={s.checkArea}>
+          <Icon name="check" width={24} height={24} />
+          <span>전체</span>
+        </div>
+        <div css={s.iconGroup}>
+          <button css={s.placeButton}>
+            <Icon name="mapFill" width={14} height={14} />
+            <span>기준배송지</span>
+          </button>
+          <button css={s.iconOnly}>
+            <Icon name="wish" width={16} height={16} />
+          </button>
+          <button css={s.iconOnly}>
+            <Icon name="trash" width={16} height={16} />
+          </button>
+          <button css={s.iconOnly}>
+            <Icon name="viewDetail" width={16} height={16} />
+          </button>
+        </div>
+      </div>
+
+      <div css={s.tabChipWrapper}>
+        <button css={s.kyoboChip}>교보문고</button>
+      </div>
+
+      <main css={s.Body}></main>
+    </div>
+  );
 };
 
 export default HomeBestDetailCart;
