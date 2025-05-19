587319134f46ed6562c95a94e856086c05eef6e2
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-05-20 03:22:39 +0900

    chore: 스타일 가장 아래서 export (#44)

--- src/pages/HomeBestDetail/EventSection/EventCard/EventCard.style.ts
+++ src/pages/HomeBestDetail/EventSection/EventCard/EventCard.style.ts
@@ -1,13 +1,13 @@
 import { css, type Theme } from '@emotion/react';
 
-export const card = css`
+const card = css`
   display: flex;
   flex-direction: column;
   width: 26rem;
   flex-shrink: 0;
 `;
 
-export const image = css`
+const image = css`
   margin-bottom: 1.2rem;
   width: 100%;
   height: 14rem;
@@ -15,12 +15,14 @@ export const image = css`
   object-fit: cover;
 `;
 
-export const title = (theme: Theme) => css`
+const title = (theme: Theme) => css`
   margin-bottom: 0.6rem;
   ${theme.fonts.body1};
 `;
 
-export const date = (theme: Theme) => css`
+const date = (theme: Theme) => css`
   color: ${theme.colors.gray4};
   ${theme.fonts.caption3};
 `;
+
+export { card, image, title, date };
