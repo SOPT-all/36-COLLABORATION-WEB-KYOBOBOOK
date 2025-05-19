17736079b4b292e288a03b1f50ef76e14134a49a
Saebom <spring180@naver.com>
2025-05-20 04:28:14 +0900

    feat: BookReview 섹션 컴포넌트 제작 (#49)

--- src/pages/HomeBestDetail/components/sections/BookReview/BookReview.tsx
+++ src/pages/HomeBestDetail/components/sections/BookReview/BookReview.tsx
@@ -1,20 +1,19 @@
+import { useTheme } from '@emotion/react';
+
 import type { ReviewTypes } from '@/types/reviewTypes';
 import { reviewData } from '@/sampleData/reviewData';
 import { ratingData } from '@/sampleData/ratingData';
 import ReviewCard from '@/pages/HomeBestDetail/components/elements/ReviewCard/ReviewCard';
 import RatingBar from '@/pages/HomeBestDetail/components/elements/RatingBar/RatingBar';
-import BookMeta from '@/pages/HomeBestDetail/components/elements/BookMeta/BookMeta';
 import HorizontalScrollList from '@/components/HorizontalScroll/HorizontalScroll';
 import TabMenu from '@/pages/HomeBestDetail/components/elements/TabMenu/TabMenu';
+import Icon from '@/components/Icon';
+import EmotionBar from '@/pages/HomeBestDetail/components/elements/EmotionBar/EmotionBar';
+import StarRating from '@/pages/HomeBestDetail/components/elements/StarRating/StarRating';
+import { bookReviewStyles as s } from '@/pages/HomeBestDetail/components/sections/BookReview/BookReivew.style'; // 👈 스타일 객체 import
 
 export default function BookReview() {
-  const bookMeta = {
-    카테고리: '한국에세이',
-    출간일: '2025.04.06',
-    쪽수: '200P',
-    크기: '9791191114768',
-    ISBN: '137 * 196 * 22mm / 397 g',
-  };
+  const theme = useTheme();
 
   const sortedStar = Object.entries(ratingData.starDistribution).sort(
     ([a], [b]) => Number(b) - Number(a),
@@ -22,26 +21,55 @@ export default function BookReview() {
 
   return (
     <>
-      <div>
-        <TabMenu type="default" />
-      </div>
-      <HorizontalScrollList gap="0" sidePadding="0">
-        {Object.entries(bookMeta).map(([title, content], index) => {
-          return <BookMeta key={title} title={title} content={content} index={index} />;
-        })}
-      </HorizontalScrollList>
+      <div css={s.wrapper}>
+        <div>
+          <div css={s.title(theme)}>
+            <h3>리뷰 ({reviewData.reviewCounts})</h3>
+            <Icon name="info" />
+          </div>
+          <p css={s.description(theme)}>* 구매 후 리뷰 작성 시, e교환권 200원 적립</p>
+        </div>
 
-      <div>
-        {sortedStar.map(([key, percent]) => {
-          const rating = Number(key);
-          return <RatingBar key={rating} rating={rating} percent={percent} />;
-        })}
+        <div>
+          <div css={s.ratingTitle(theme)}>
+            <StarRating rating={ratingData.averageStar} width={26} height={26} />
+            <span>
+              <b css={s.ratingText(theme)}>{ratingData.averageStar}</b>/5
+            </span>
+          </div>
+          {sortedStar.map(([key, percent]) => {
+            const rating = Number(key);
+            return <RatingBar key={rating} rating={rating} percent={percent} />;
+          })}
+        </div>
+
+        <div css={s.barWrapper}>
+          <p css={s.reviewText(theme)}>
+            28%의 구매자가 <br />
+            <span css={s.response}>
+              <Icon name="quote" fill={theme.colors.purple7} />
+              <b css={s.perfect(theme)}>최고예요 </b> 라고 응답했어요
+            </span>
+          </p>
+          {Object.entries(ratingData.emotionDistribution).map(([emotion, percent]) => (
+            <EmotionBar key={emotion} emotion={emotion} percent={percent} />
+          ))}
+        </div>
+        <button>리뷰작성</button>
       </div>
-      <HorizontalScrollList gap="1rem" sidePadding="0" effect={true}>
-        {reviewData.reviewList.map((review: ReviewTypes) => (
-          <ReviewCard key={review.id} reviewData={review} />
-        ))}
-      </HorizontalScrollList>
+
+      <TabMenu type="review" />
+      <div css={s.cardWrapper}>
+        <HorizontalScrollList gap="1rem" sidePadding="0" effect={true}>
+          {reviewData.reviewList.map((review: ReviewTypes) => (
+            <ReviewCard key={review.id} reviewData={review} />
+          ))}
+        </HorizontalScrollList>
+      </div>
+      <div css={s.moreWrapper}>
+        <span css={s.moreText(theme)}>더보기 1/12</span>
+        <Icon name="circleDown" fill={theme.colors.white} />
+      </div>
     </>
   );
 }
