8856dba1cf86294a6a5fa9d6c75ab22a7e43176d
봄 <saebom@Saebomui-MacBookPro.local>
2025-05-19 17:36:12 +0900

    feat: RatingBar 컴포넌트 제작 (#33)

--- src/pages/HomeBestDetail/HomeBestDetail.tsx
+++ src/pages/HomeBestDetail/HomeBestDetail.tsx
@@ -1,5 +1,19 @@
+import RatingBar from '@/pages/HomeBestDetail/RatingBar/RatingBar';
+import { data } from '@/pages/HomeBestDetail/EmotionBar/ReviewData';
+
 const HomeBestDetail = () => {
-  return <></>;
+  const sortedStar = Object.entries(data.starDistribution).sort(
+    ([a], [b]) => Number(b) - Number(a),
+  );
+
+  return (
+    <>
+      {sortedStar.map(([key, percent]) => {
+        const rating = Number(key);
+        return <RatingBar key={rating} rating={rating} percent={percent} />;
+      })}
+    </>
+  );
 };
 
 export default HomeBestDetail;
