9ef54ca9a9112e85d0d9b5c57825ff2c7af88302
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-05-20 02:00:26 +0900

    chore: 소문자 ... 때문에 빌드 오류인듯 (#44)

--- src/pages/HomeBestDetail/KeywordSection/KeywordSection.constants.ts
+++ src/pages/HomeBestDetail/KeywordSection/KeywordSection.constants.ts
@@ -1,7 +1,7 @@
-import keywordImage1 from '@/assets/img/KeywordImage1.jpg';
-import keywordImage2 from '@/assets/img/KeywordImage2.jpg';
-import keywordImage3 from '@/assets/img/KeywordImage3.jpg';
-import keywordImage4 from '@/assets/img/KeywordImage4.jpg';
+import keywordImage1 from '@/assets/img/keywordImage1.jpg';
+import keywordImage2 from '@/assets/img/keywordImage2.jpg';
+import keywordImage3 from '@/assets/img/keywordImage3.jpg';
+import keywordImage4 from '@/assets/img/keywordImage4.jpg';
 
 const FILTER_BUTTON_LIST = ['인생', '영하', '연재당시', '가족사', '날씨', '사적'];
 const KEYWORD_IMAGE_LIST = [keywordImage1, keywordImage2, keywordImage3, keywordImage4];
