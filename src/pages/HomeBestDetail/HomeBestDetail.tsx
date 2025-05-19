5779afb44a943ac9b217d6efd65f935d1ec75ed8
봄 <saebom@Saebomui-MacBookPro.local>
2025-05-19 15:35:15 +0900

    feat: 빌드 문제 해결 (#31)

--- src/pages/HomeBestDetail/HomeBestDetail.tsx
+++ src/pages/HomeBestDetail/HomeBestDetail.tsx
@@ -1,8 +1,3 @@
-import { css } from '@emotion/react';
-
-import EmotionBar from '@/pages/HomeBestDetail/EmotionBar/EmotionBar';
-import { data } from '@/pages/HomeBestDetail/EmotionBar/ReviewData';
-
 const HomeBestDetail = () => {
   return <></>;
 };
