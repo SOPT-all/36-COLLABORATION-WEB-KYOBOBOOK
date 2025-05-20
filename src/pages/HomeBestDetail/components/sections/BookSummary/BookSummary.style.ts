13b965029156e16f5989f2e2fed52c1204c51e12
Saebom <spring180@naver.com>
2025-05-20 17:19:22 +0900

    feat: 책 정보 섹션 퍼블리싱 (#54)

--- src/pages/HomeBestDetail/components/sections/BookSummary/BookSummary.style.ts
+++ src/pages/HomeBestDetail/components/sections/BookSummary/BookSummary.style.ts
@@ -35,6 +35,69 @@ export const previewText = (theme: Theme) => css`
 export const currentPage = (theme: Theme) => css`
   ${theme.fonts.caption2}
 `;
+
+export const bookDetail = (theme: Theme) => css`
+  padding: 2.8rem 2.4rem;
+`;
+
+export const tagContainer = (theme: Theme) => css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.6rem;
+  margin-bottom: 1.2rem;
+`;
+
+// 칩 오면 수정
+export const tag = css`
+  padding: 0.2rem 0.6rem;
+  border: 1px solid black;
+  border-radius: 4px;
+`;
+
+export const titleText = (theme: Theme) => css`
+  padding-bottom: 0.8rem;
+  ${theme.fonts.header1};
+`;
+
+export const publisherText = (theme: Theme) => css`
+  padding-bottom: 1.2rem;
+  color: ${theme.colors.gray4};
+  ${theme.fonts.subtitle3};
+`;
+
+export const authorText = (theme: Theme) => css`
+  padding-bottom: 0.6rem;
+  ${theme.fonts.subtitle3};
+`;
+
+export const dateText = (theme: Theme) => css`
+  padding-bottom: 1.6rem;
+  color: ${theme.colors.gray4};
+  ${theme.fonts.body3};
+`;
+
+export const bestContainer = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+`;
+
+export const bestText = (theme: Theme) => css`
+  ${theme.fonts.subtitle4};
+`;
+
+export const rankContainer = (theme: Theme) => css`
+  display: flex;
+  gap: 0.4rem;
+  padding-left: 1.2rem;
+  color: ${theme.colors.gray5};
+  ${theme.fonts.caption3};
+`;
+
+export const rankText = (theme: Theme) => css`
+  color: ${theme.colors.black1};
+  ${theme.fonts.caption2};
+`;
 //export const imageBackground = (theme: Theme) => css``;
 //export const imageBackground = (theme: Theme) => css``;
 //export const imageBackground = (theme: Theme) => css``;
--- src/pages/HomeBestDetail/components/sections/BookSummary/BookSummary.tsx
+++ src/pages/HomeBestDetail/components/sections/BookSummary/BookSummary.tsx
@@ -8,13 +8,26 @@ import {
   previewText,
   currentPage,
   imageStyle,
+  bookDetail,
+  tagContainer,
+  tag,
+  titleText,
+  publisherText,
+  authorText,
+  dateText,
+  bestContainer,
+  bestText,
+  rankContainer,
+  rankText,
 } from '@/pages/HomeBestDetail/components/sections/BookSummary/BookSummary.style';
+import Icon from '@/components/Icon';
+import Divider from '@/components/Divider/Divider';
 
 const BookSummary = () => {
   const theme = useTheme();
 
   return (
-    <div>
+    <>
       <div css={imageBackground(theme)}>
         <div css={bookContainer}>
           <img css={imageStyle(theme)} src={BookImage} alt="책 대표 이미지" />
@@ -26,7 +39,39 @@ const BookSummary = () => {
           </div>
         </div>
       </div>
-    </div>
+
+      <div css={bookDetail(theme)}>
+        <div css={tagContainer(theme)}>
+          <span css={tag}>2025년 04월 04일 오늘의 선택</span>
+          <span css={tag}>MD의 선택</span>
+          <span css={tag}>무료배송</span>
+          <span css={tag}>사은품</span>
+          <span css={tag}>이벤트</span>
+          <span css={tag}>소득공제</span>
+        </div>
+
+        <h3 css={titleText(theme)}>단 한번의 삶</h3>
+        <p css={publisherText(theme)}>양장본 Hardcover</p>
+        <p css={authorText(theme)}>김영하 저자(글)</p>
+        <p css={dateText(theme)}>복복서가 · 2025년 04월 06일</p>
+
+        <div css={bestContainer(theme)}>
+          <Icon name="best" />
+          <strong css={bestText(theme)}>주간베스트</strong>
+          <div css={rankContainer(theme)}>
+            <span>
+              국내도서 <b css={rankText(theme)}>2</b>위
+            </span>
+            <span>·</span>
+            <span>
+              시/에세이 <b>1</b>위
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <Divider />
+    </>
   );
 };
 
