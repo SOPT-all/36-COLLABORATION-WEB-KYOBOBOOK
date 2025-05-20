bd4329a8d75d9d7236849e97cea20d06595d4197
Saebom <spring180@naver.com>
2025-05-20 19:00:12 +0900

    feat: ProductDetail 섹션 컴포넌트 퍼블리싱 (#54)

---
+++ src/pages/HomeBestDetail/components/sections/BookSummary/ProductDetail.style.tsx
@@ -0,0 +1,188 @@
+import type { Theme } from '@emotion/react';
+import { css } from '@emotion/react';
+
+export const productWrapper = (theme: Theme) => css`
+  padding: 0 2.4rem;
+`;
+
+export const priceWrapper = (theme: Theme) => css`
+  margin: 2.6rem 0 1.6rem 0;
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const saleText = (theme: Theme) => css`
+  color: ${theme.colors.green1};
+  ${theme.fonts.title3};
+`;
+
+export const priceText = (theme: Theme) => css`
+  ${theme.fonts.title2};
+`;
+
+export const priceNumber = (theme: Theme) => css`
+  ${theme.fonts.title1};
+`;
+
+export const beforeText = (theme: Theme) => css`
+  text-decoration: line-through;
+  color: ${theme.colors.gray4};
+  ${theme.fonts.body3};
+`;
+
+export const benefitWrapper = (theme: Theme) => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.6rem 0;
+  border-top: 1px solid ${theme.colors.gray2};
+`;
+
+export const benefitText = (theme: Theme) => css`
+  ${theme.fonts.subtitle4};
+`;
+
+export const pointWrapper = (theme: Theme) => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+export const pointText = (theme: Theme) => css`
+  color: ${theme.colors.green1};
+  ${theme.fonts.subtitle4};
+`;
+
+export const deliverWrapper = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.6rem 0;
+  border-top: 1px solid ${theme.colors.gray2};
+`;
+
+export const deliverFistWrapper = (theme: Theme) => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const deliverRightWrapper = (theme: Theme) => css`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+// 칩 오면 수정
+export const deliverChip = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  padding: 0 0.8rem;
+  height: 2rem;
+  border-radius: 4rem;
+  background-color: ${theme.colors.purple1};
+  color: ${theme.colors.purple7};
+  ${theme.fonts.caption3};
+`;
+
+export const deliverText = (theme: Theme) => css`
+  ${theme.fonts.body2};
+`;
+
+export const arriveText = (theme: Theme) => css`
+  ${theme.fonts.subtitle4};
+`;
+
+export const deliverInfoWrapper = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
+  padding: 1.6rem 0;
+  border-top: 1px solid ${theme.colors.gray2};
+`;
+
+export const deliverInfoText = (theme: Theme) => css`
+  text-align: right;
+  ${theme.fonts.body2};
+`;
+
+export const loginText = (theme: Theme) => css`
+  text-decoration: underline;
+  color: ${theme.colors.green1};
+  ${theme.fonts.caption2};
+`;
+
+export const infoWrapper = (theme: Theme) => css`
+  padding: 1.2rem;
+  width: 100%;
+  background-color: ${theme.colors.gray1};
+  border-radius: 8px;
+`;
+
+export const firstLineContainer = (theme: Theme) => css`
+  display: flex;
+  gap: 0.2rem;
+  align-items: center;
+`;
+
+export const firstLineText = (theme: Theme) => css`
+  ${theme.fonts.body3};
+`;
+
+export const moreButton = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+  padding: 0 0.2rem 0 0.6rem;
+  margin-left: auto;
+  height: 2.4rem;
+  background-color: white;
+  border: 1px solid ${theme.colors.gray3};
+  border-radius: 4px;
+`;
+
+export const moreText = (theme: Theme) => css`
+  ${theme.fonts.caption2};
+`;
+
+export const secondLineText = (theme: Theme) => css`
+  color: ${theme.colors.gray4};
+  ${theme.fonts.caption3};
+`;
+
+export const alarmInfoContainer = (theme: Theme) => css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const alarmText = (theme: Theme) => css`
+  width: 22rem;
+  color: ${theme.colors.gray5};
+  ${theme.fonts.caption3};
+`;
+
+export const alarmButton = (theme: Theme) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.2rem;
+  padding: 0 0.8rem;
+  height: 2.8rem;
+  border: 1px solid ${theme.colors.purple7};
+  border-radius: 4px;
+  background-color: ${theme.colors.white};
+`;
+
+export const alarmButtonText = (theme: Theme) => css`
+  color: ${theme.colors.purple7};
+  ${theme.fonts.caption3};
+`;
+
+export const alarmSecondLineText = (theme: Theme) => css`
+  margin-top: 0.8rem;
+  color: ${theme.colors.gray5};
+  ${theme.fonts.caption3};
+`;
--- src/pages/HomeBestDetail/components/sections/BookSummary/ProductDetail.tsx
+++ src/pages/HomeBestDetail/components/sections/BookSummary/ProductDetail.tsx
@@ -1,5 +1,117 @@
+import { useTheme } from '@emotion/react';
+
+import {
+  productWrapper,
+  priceWrapper,
+  saleText,
+  priceText,
+  priceNumber,
+  beforeText,
+  benefitWrapper,
+  benefitText,
+  pointWrapper,
+  pointText,
+  deliverText,
+  deliverWrapper,
+  deliverFistWrapper,
+  deliverRightWrapper,
+  deliverChip,
+  arriveText,
+  deliverInfoWrapper,
+  deliverInfoText,
+  loginText,
+  infoWrapper,
+  firstLineContainer,
+  firstLineText,
+  moreButton,
+  moreText,
+  secondLineText,
+  alarmInfoContainer,
+  alarmText,
+  alarmButton,
+  alarmButtonText,
+  alarmSecondLineText,
+} from '@/pages/HomeBestDetail/components/sections/BookSummary/ProductDetail.style';
+import Icon from '@/components/Icon';
+
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  const theme = useTheme();
+
+  return (
+    <div css={productWrapper(theme)}>
+      <div css={priceWrapper(theme)}>
+        <span css={saleText(theme)}>10%</span>
+        <span css={priceText(theme)}>
+          <b css={priceNumber(theme)}>15,120</b>원
+        </span>
+        <span css={beforeText(theme)}>16,800원</span>
+      </div>
+
+      <div css={benefitWrapper(theme)}>
+        <p css={benefitText(theme)}>적립/혜택</p>
+        <div css={pointWrapper(theme)}>
+          <span css={pointText(theme)}>840P</span>
+          <Icon name="circleDown" fill={theme.colors.white} width={20} height={20} />
+        </div>
+      </div>
+
+      <div css={deliverWrapper(theme)}>
+        <div css={deliverFistWrapper(theme)}>
+          <p css={benefitText(theme)}>배송안내</p>
+          <div css={deliverRightWrapper(theme)}>
+            <span css={deliverText(theme)}>무료배송</span>
+            <Icon name="info" fill={theme.colors.white} width={18} height={18} />
+          </div>
+        </div>
+        <div css={deliverRightWrapper(theme)}>
+          <span css={deliverChip(theme)}>당일배송</span>
+          <b css={arriveText(theme)}>오늘 (4/24,목) 도착</b>
+          <Icon name="info" fill={theme.colors.white} width={18} height={18} />
+        </div>
+        <div css={deliverRightWrapper(theme)}>
+          <span css={deliverText(theme)}>기본배송지 기준</span>
+          <Icon name="info" fill={theme.colors.white} width={18} height={18} />
+        </div>
+      </div>
+
+      <div css={deliverInfoWrapper(theme)}>
+        <p css={deliverInfoText(theme)}>
+          <span css={loginText(theme)}>로그인</span> 후
+          <span css={arriveText(theme)}> 정확한 배송 안내</span>를 받아보세요!
+        </p>
+
+        <div css={infoWrapper(theme)}>
+          <div css={firstLineContainer(theme)}>
+            <p css={firstLineText(theme)}>이달의 꽃과 함께 책을 받아보세요!</p>
+            <button css={moreButton(theme)}>
+              <span css={moreText(theme)}>자세히 보기</span>
+              <Icon name="right" width={10} height={10} fill={theme.colors.gray4} />
+            </button>
+          </div>
+          <p css={secondLineText(theme)}>· 1권 구매 시 결제 단계에서 적용 가능합니다.</p>
+        </div>
+      </div>
+
+      <div css={deliverInfoWrapper(theme)}>
+        <div css={alarmInfoContainer(theme)}>
+          <p css={alarmText(theme)}>알림 신청하시면 원하시는 정보를 받아 보실 수 있습니다.</p>
+          <button css={alarmButton(theme)}>
+            <Icon name="bell" width={10} height={10} fill={theme.colors.purple7} />
+            <span css={alarmButtonText(theme)}>알림신청</span>
+          </button>
+        </div>
+        <div css={infoWrapper(theme)}>
+          <div css={firstLineContainer(theme)}>
+            <Icon name="message" width={20} height={20} />
+            <p css={arriveText(theme)}>알립니다.</p>
+          </div>
+          <p css={alarmSecondLineText(theme)}>
+            사은품 ‘영하의 무물' 한정판 별책 (포인트 차감, 추가결제)
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
