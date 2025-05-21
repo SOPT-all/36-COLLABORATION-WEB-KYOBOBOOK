8de03b1e2f0d420b88c3b497b837d451fdec41ee
승우 <hsu235@naver.com>
2025-05-22 01:46:13 +0900

    feat: homebestdetailcart 페이지 퍼블리싱 완료 (#65)
    feat: homebestdetailcart 페이지 퍼블리싱 완료 (#65)

---
+++ src/pages/HomeBestDetailCart/ThirdCartView/ThirdCartView.style.ts
@@ -0,0 +1,45 @@
+import { css, type Theme } from '@emotion/react';
+
+const Wrapper = (theme: Theme) => css`
+  width: 100%;
+  max-width: 73rem;
+  min-width: 37.5rem;
+  background-color: ${theme.colors.white};
+  flex-direction: column;
+  padding: 2.4rem 1.6rem;
+`;
+
+const cardWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 1.2rem 0rem;
+`;
+
+const cartButton = (theme: Theme) => css`
+  padding: 0.6rem 2rem;
+  background-color: ${theme.colors.purple6};
+  color: ${theme.colors.gray6};
+  border: 1px solid ${theme.colors.gray2};
+  border-radius: 4px;
+  font: ${theme.fonts.caption2};
+  cursor: pointer;
+`;
+
+const title = (theme: Theme) => css`
+  font: ${theme.fonts.subtitle4};
+  color: ${theme.colors.black1};
+`;
+
+const price = (theme: Theme) => css`
+  font: ${theme.fonts.caption1};
+  color: ${theme.colors.black1};
+`;
+
+export const ThirdCartViewStyle = {
+  Wrapper,
+  cardWrapper,
+  cartButton,
+  title,
+  price,
+};
